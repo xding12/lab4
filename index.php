1d949d4b1e7a92cea26cd94179e117f108a7fd86
akshaydesai <adesai16>
2019-02-10 18:19:14 -0500

    index added

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<title>Home | Smash Site</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body class="container">
+		<header>
+			<h1>Here is the index of all our interesting options</h1>
+		</header>
+
+		<aside>
+			<ul>
+				<li>Streaming Services</li>
+				<li>Object Oriented Languages</li>
+				<li>Philosophers</li>
+			</ul>
+		</aside>
+
+		<main>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rutrum at mauris eget iaculis. In massa odio, scelerisque sit amet viverra vel, porta at purus. Nulla porttitor orci id nibh efficitur, ullamcorper posuere justo accumsan. Phasellus sagittis vulputate augue in porttitor. Aliquam gravida odio facilisis quam tincidunt pulvinar. Vivamus sed magna sit amet lectus luctus varius eget eget neque. Vivamus porta nisl a urna semper pretium. Maecenas semper volutpat orci eget egestas. Cras magna nisl, bibendum at tortor sit amet, vehicula efficitur nisi. Aenean vel dignissim mi. Nullam mauris massa, consequat eget enim quis, molestie aliquet mi.
+
+			Praesent congue eros lorem, ac pulvinar enim commodo ornare. Aliquam et nibh et arcu dignissim feugiat eget nec ex. Duis non elit non tellus tristique bibendum. Integer vulputate orci ut justo dignissim rhoncus. Suspendisse sodales, libero nec faucibus ultricies, nibh metus vehicula tortor, id pellentesque neque lectus et dui. Integer porttitor est eget sem luctus placerat. Curabitur tempor felis eu accumsan vestibulum. Cras sed volutpat nibh. Nullam quis lorem nisl. Pellentesque sollicitudin magna sit amet mi ornare condimentum. Mauris odio leo, molestie sed volutpat eu, viverra ultricies dui. Morbi felis lorem, faucibus id sem sit amet, volutpat aliquam diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+
+			Quisque interdum gravida tellus mollis pretium. Vivamus sed nisl mattis, dictum erat vestibulum, cursus libero. Pellentesque maximus tellus mauris. Nunc porttitor purus et metus euismod, at elementum odio semper. Proin a risus tempor, sagittis orci at, cursus nisl. Nulla nec elementum mi. Curabitur placerat odio lectus, quis sollicitudin tortor pretium hendrerit. Aenean faucibus quam metus, non posuere tellus bibendum et. Phasellus venenatis dolor eget purus lacinia, vitae tempus ligula eleifend. Vestibulum dui libero, lacinia nec porttitor in, venenatis vel erat. Integer quis congue ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet urna maximus, luctus mauris non, sagittis neque. <p>
+
+		</main>
+
+		<?php include "includes/footnav.inc"?>
